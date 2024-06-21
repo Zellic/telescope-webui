795db3a0320bad265cf6dd4f556a60dd33bcdcde
Sirenfal <avosirenfal@gmail.com>
2024-06-21 18:39:07 -0400

    * Provide code button

--- components/tg/account-table.tsx
+++ components/tg/account-table.tsx
@@ -1,14 +1,16 @@
 "use client";
-import React from "react";
+import React, { Fragment } from "react";
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, getKeyValue} from "@nextui-org/react";
 import { DeleteIcon, EditIcon, EyeIcon } from "@nextui-org/shared-icons";
 import { CheckIcon } from "@nextui-org/shared-icons";
 import { TelegramAccount } from "@/components/tg/account-table-types";
+import { Button } from "@nextui-org/button";
 
 const columns = [
 	{name: "NAME", uid: "name"},
 	// {name: "ROLE", uid: "role"},
 	{name: "STATUS", uid: "status"},
+	{name: "AUTHENTICATION", uid: "authentication"},
 	{name: "ACTIONS", uid: "actions"},
 ];
 
@@ -50,25 +52,33 @@ export function AccountTable(props: {users: TelegramAccount[]}) {
 					<Chip className="capitalize" color={status as any} size="sm" variant="flat">
 						{user.status.stage}
 					</Chip>
-				);
+				)
+			case "authentication":
+				if(status === "success")
+					return null;
+				return (
+					<Button isLoading={status === "warning"}>
+						Provide
+					</Button>
+				)
 			case "actions":
 				return (
 					<div className="relative flex items-center gap-2">
-						<Tooltip content="Details">
-							<span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-								<EyeIcon />
-							</span>
-						</Tooltip>
-						<Tooltip content="Edit user">
-							<span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-								<EditIcon />
-							</span>
-						</Tooltip>
-						<Tooltip color="danger" content="Delete user">
-							<span className="text-lg text-danger cursor-pointer active:opacity-50">
-								<DeleteIcon />
-							</span>
-						</Tooltip>
+						{/*<Tooltip content="Details">*/}
+						{/*	<span className="text-lg text-default-400 cursor-pointer active:opacity-50">*/}
+						{/*		<EyeIcon />*/}
+						{/*	</span>*/}
+						{/*</Tooltip>*/}
+						{/*<Tooltip content="Edit user">*/}
+						{/*	<span className="text-lg text-default-400 cursor-pointer active:opacity-50">*/}
+						{/*		<EditIcon />*/}
+						{/*	</span>*/}
+						{/*</Tooltip>*/}
+						{/*<Tooltip color="danger" content="Delete user">*/}
+						{/*	<span className="text-lg text-danger cursor-pointer active:opacity-50">*/}
+						{/*		<DeleteIcon />*/}
+						{/*	</span>*/}
+						{/*</Tooltip>*/}
 					</div>
 				);
 		}
@@ -88,6 +98,7 @@ export function AccountTable(props: {users: TelegramAccount[]}) {
 					<TableRow key={item.phone}>
 						<TableCell>{renderCell(item, "name")}</TableCell>
 						<TableCell>{renderCell(item, "status")}</TableCell>
+						<TableCell>{renderCell(item, "authentication")}</TableCell>
 						<TableCell>{renderCell(item, "actions")}</TableCell>
 					</TableRow>
 				)}
