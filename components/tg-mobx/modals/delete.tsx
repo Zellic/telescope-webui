ad948935e8c1aa892c6250ca6f7e1f2b64c27d6e
rose <47357290+rose4096@users.noreply.github.com>
2024-09-27 12:32:59 -0400

    Make deleting a user a blocking modal

--- components/tg-mobx/modals/delete.tsx
+++ components/tg-mobx/modals/delete.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite";
 import { useTelegramStore } from "@/components/models/telegram";
 import BasicModal from "@/components/tg-mobx/modals/modal";
 import React, { useState } from "react";
-import { Button } from "@nextui-org/react";
+import { Button, Spinner } from "@nextui-org/react";
 import { Input } from "@nextui-org/input";
 import { ApiService } from "@/components/api";
 import { NameCell } from "@/components/tg-mobx/account-table";
@@ -10,6 +10,7 @@ import { NameCell } from "@/components/tg-mobx/account-table";
 export const DeleteAccountModal = observer(() => {
 	const telegramStore = useTelegramStore();
 	const [value, setValue] = useState("");
+	const [submitting, setSubmitting] = useState(false);
 
 	const isOpen = telegramStore.modals.deleteClient !== null;
 
@@ -24,56 +25,72 @@ export const DeleteAccountModal = observer(() => {
 			onClose={onClose}
 			header={"Remove account"}
 			body={
-				<>
-					<p>Really remove this account from Telescope and delete all associated data?</p>
-					<div className="flex items-center ml-6">
-						<NameCell account={telegramStore.modals.deleteClient!} />
-					</div>
-				</>
+				submitting ?
+					<>
+						<p>Deleting...</p>
+					</>
+					:
+					<>
+						<p>Really remove this account from Telescope and delete all associated data?</p>
+						<div className="flex items-center ml-6">
+							<NameCell account={telegramStore.modals.deleteClient!} />
+						</div>
+					</>
 			}
 			footer={
-				<div className="w-full flex flex-col gap-3">
-					<div>
-						<Input
-							isRequired={true}
-							type="text"
-							label="Confirm"
-							placeholder="Type DELETE to confirm..."
-							labelPlacement="outside"
-							maxLength={6}
-							value={value}
-							onValueChange={(value) => {
-								setValue(value.toUpperCase());
-							}}
-						/>
-					</div>
-					<div className="flex flex-row gap-2">
-						<Button color="danger" isDisabled={value.toUpperCase() != "DELETE"} onPress={() => {
-							setValue("");
+				submitting ?
+					<>
+						<Spinner/>
+					</>
+					:
+					<>
+						<div className="w-full flex flex-col gap-3">
+							<div>
+								<Input
+									isRequired={true}
+									type="text"
+									label="Confirm"
+									placeholder="Type DELETE to confirm..."
+									labelPlacement="outside"
+									maxLength={6}
+									value={value}
+									onValueChange={(value) => {
+										setValue(value.toUpperCase());
+									}}
+								/>
+							</div>
+							<div className="flex flex-row gap-2">
+								<Button color="danger" isDisabled={value.toUpperCase() != "DELETE"} onPress={() => {
+									setValue("");
+									setSubmitting(true);
 
-							(async function(phone) {
-								const result = await ApiService.getInstance().deleteaccount(phone);
+									const phone = telegramStore.modals.deleteClient!.phone;
+									ApiService.getInstance().deleteaccount(phone).then(res => {
+										setSubmitting(false);
+										telegramStore.modals.setDeleteClient(null);
+										if (!res.success) {
+											telegramStore.modals.setMessageBasic(
+												"Error",
+												`Failed to delete account: ${res.error}`
+											);
+										}
+									}).catch(res => {
+										setSubmitting(false);
+										telegramStore.modals.setDeleteClient(null);
+										telegramStore.modals.setMessageBasic(
+											"Error", "Failed to reach server"
+										);
+									});
 
-								// TODO: this doesn't actually happen, it's async serverside. should probably change this?
-								// problem is client shutdown can take a bit...
-								if (!result.success) {
-									telegramStore.modals.setMessageBasic(
-										"Error",
-										`Failed to delete account: ${result.error}`
-									);
-								}
-							})(telegramStore.modals.deleteClient!.phone);
-
-							telegramStore.modals.setDeleteClient(null);
-
-						}}>
-							Delete User
-						</Button>
-						<Button color="default" onPress={onClose}>
-							Cancel
-						</Button>
-					</div>
-				</div>
+								}}>
+									Delete User
+								</Button>
+								<Button color="default" onPress={onClose}>
+									Cancel
+								</Button>
+							</div>
+						</div>
+					</>
 			}
 		/>
 	);
