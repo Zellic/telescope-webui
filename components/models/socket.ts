c9da20a7131b20d3528cb5c56749d9c0f46a70cf
rose <47357290+rose4096@users.noreply.github.com>
2024-10-02 12:30:31 -0400

    Commit before fixing branch and add readme back (accident)

--- components/models/socket.ts
+++ components/models/socket.ts
@@ -1,3 +1,16 @@
-export interface SocketMessage {
-	type: 'CLIENT_START',
+import { IEnvironment, ITelegramAccount } from "@/components/models/telegram";
+
+export enum MessageType {
+	CLIENT_START = 'CLIENT_START',
 }
+
+export interface ClientStartMessage {
+	type: MessageType.CLIENT_START;
+	data: {
+		hash: string;
+		environment: IEnvironment;
+		items: Array<ITelegramAccount> | undefined
+	};
+}
+
+export type SocketMessage = ClientStartMessage /* | MORE TYPES>.. */;
