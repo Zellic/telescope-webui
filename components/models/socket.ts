9f9d104577b94c52ac966fcba61afcdbcb458fcc
rose <47357290+rose4096@users.noreply.github.com>
2024-10-02 19:24:22 -0400

    More socket conversion

--- components/models/socket.ts
+++ components/models/socket.ts
@@ -1,12 +1,14 @@
 import { IEnvironment, ITelegramAccount } from "@/components/models/telegram";
 import { getRoot, types } from "mobx-state-tree";
 
-export enum MessageType {
+export enum MessageRecvType {
 	CLIENT_START = "CLIENT_START",
+	ADD_TEST_ACCOUNT_RESPONSE = "ADD_TEST_ACCOUNT_RESPONSE",
+	SUBMIT_VALUE_RESPONSE = "SUBMIT_VALUE_RESPONSE"
 }
 
-export interface ClientStartMessage {
-	type: MessageType.CLIENT_START;
+export interface ClientStart {
+	type: MessageRecvType.CLIENT_START;
 	data: {
 		hash: string;
 		environment: IEnvironment;
@@ -14,7 +16,42 @@ export interface ClientStartMessage {
 	};
 }
 
-export type SocketMessage = ClientStartMessage /* | MORE TYPES>.. */;
+export interface GenericResponse {
+	data: {
+		status: "OK" | "ERROR",
+		error: null | string;
+	};
+}
+
+export type AddTestAccountResponse = {
+	type: MessageRecvType.ADD_TEST_ACCOUNT_RESPONSE;
+} & GenericResponse;
+
+export type SubmitValueResponse = {
+	type: MessageRecvType.SUBMIT_VALUE_RESPONSE;
+} & GenericResponse;
+
+export type SocketRecvMessage = ClientStart | AddTestAccountResponse | SubmitValueResponse;
+
+export enum MessageSendType {
+	ADD_TEST_ACCOUNT = "ADD_TEST_ACCOUNT",
+	SUBMIT_VALUE = "SUBMIT_VALUE",
+}
+
+export interface AddTestAccount {
+	type: MessageSendType.ADD_TEST_ACCOUNT;
+}
+
+export interface SubmitValue {
+	type: MessageSendType.SUBMIT_VALUE;
+	data: {
+		phone: string;
+		stage: string;
+		value: string;
+	};
+}
+
+export type SocketSendMessage = AddTestAccount | SubmitValue;
 
 export const WebSocketStore = types
 	.model({
@@ -24,7 +61,7 @@ export const WebSocketStore = types
 		socket: null as WebSocket | null
 	}))
 	.actions((self) => ({
-		setState(state: "connecting" | "open" | "closed" | "error")  {
+		setState(state: "connecting" | "open" | "closed" | "error") {
 			self.socketState = state;
 		},
 
@@ -33,7 +70,7 @@ export const WebSocketStore = types
 				self.socket.close();
 			}
 
-			this.setState("connecting")
+			this.setState("connecting");
 			self.socket = new WebSocket(url);
 
 			self.socket.onopen = () => {
@@ -50,21 +87,38 @@ export const WebSocketStore = types
 
 			self.socket.onmessage = (event) => {
 				const message = JSON.parse(event.data);
-				if (message.hasOwnProperty('type')) {
+				if (message.hasOwnProperty("type")) {
 					// @ts-ignore: dont want to include the type (circular)
 					getRoot(self).updateFromSocket(message);
 				} else {
-					console.error("WebSocket data must contain an `id` field")
+					console.error("WebSocket data must contain an `id` field");
 				}
 			};
 		},
 
-		sendMessage(data: any) {
+		sendMessage(data: SocketSendMessage) {
 			if (self.socket && self.socket.readyState === WebSocket.OPEN) {
 				self.socket.send(JSON.stringify(data));
 			} else {
 				console.error(`Failed to send message: WebSocket is closed`);
 			}
+		},
+
+		addTestAccount() {
+			this.sendMessage({
+				type: MessageSendType.ADD_TEST_ACCOUNT
+			});
+		},
+
+		submitValue(phone: string, stage: string, value: string) {
+			this.sendMessage({
+				type: MessageSendType.SUBMIT_VALUE,
+				data: {
+					phone,
+					stage,
+					value
+				}
+			});
 		},
 
 		disconnect() {
